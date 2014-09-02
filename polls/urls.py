746f95d25feb062c7a57114b86039742384c1433
Marc Prewitt <marc@chartbeat.com>
2014-09-02 13:02:31 -0400

    Finished chapter 5.

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,8 @@ from polls import views
 
 urlpatterns = patterns(
     '',
-    # eg: /polls/
-    url(r'^$', views.index, name='index'),
-    # eg: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    # eg: /polls/5/results/
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    # eg: /polls/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
